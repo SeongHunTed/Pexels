e622476cabb3e9000babbdce05bbe1d600f52fec
김성훈 <42074365+SeongHunTed@users.noreply.github.com>
2025-05-10 18:14:05 +0900

    Project Setting

--- Pexels/Extension/Dictionary+Extension.swift
+++ Pexels/Extension/Dictionary+Extension.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Dictionary where Key == String, Value == String {
 	/// 두 개의 [String: String]을 합친다.
 	/// - parameter other: 덮어쓸(우선시할) 딕셔너리
-	/// - returns: self + other 를 머지한 결과
+	/// - returns: self + other 를 머지한 결과  
 	func deepMerged(with other: [String: String]) -> [String: String] {
 		var merged = self
 		other.forEach { merged[$0] = $1 } // 동일 키면 other 값으로 덮어쓰기
